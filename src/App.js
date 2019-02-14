8fe4601449b42f06bb3a41bb6eafdca5ab0c3d66
Susana <46267460+sujacquin@users.noreply.github.com>
2019-02-14 11:06:43 +0100

    added todo-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -10,7 +11,77 @@ library.add(faTimesCircle)
 class App extends Component {
   state = {
     title: "",
-    titleList: []
+    titleList: [],
+    dragStart: "",
+    drop: ""
+  }
+
+
+  dragStart = async (index) => {
+
+    await this.setState({ dragStart: index });
+    console.log("drag", index);
+
+
+  }
+
+
+  drop = async (index) => {
+    try {
+      if (index === "last") {
+        await this.setState({ drop: this.state.titleList.length - 1 })
+      } else { await this.setState({ drop: index }) }
+
+      const taskArray = [...this.state.titleList];
+      const array = [];
+
+      const dragged = this.state.titleList[this.state.dragStart];
+
+      // taskArray.splice(this.state.dragStart, 1);
+
+
+      // taskArray[this.state.drop] = dragged
+
+
+      for (let i = 0; i < this.state.dragStart; i++) {
+        array[i] = taskArray[i]
+      }
+
+      array[this.state.drop] = dragged;
+
+      if (this.state.dragStart < this.state.drop) {
+        for (let i = 0; i < this.state.dragStart; i++) {
+          array[i] = taskArray[i]
+        }
+
+        for (let i = this.state.dragStart; i < this.state.drop; i++) {
+          array[i] = taskArray[i + 1]
+        }
+        for (let i = this.state.drop + 1; i < taskArray.length; i++) {
+          array[i] = taskArray[i]
+        }
+
+      }
+
+      if (this.state.dragStart > this.state.drop) {
+        for (let i = 0; i < this.state.drop; i++) {
+          array[i] = taskArray[i]
+        }
+        for (let i = this.state.drop + 1; i <= this.state.dragStart; i++) {
+          array[i] = taskArray[i - 1]
+        }
+        for (let i = this.state.dragStart + 1; i < taskArray.length; i++) {
+          array[i] = taskArray[i]
+        }
+
+      }
+
+
+      await this.setState({ titleList: array })
+    } catch (error) {
+      console.log(error.message);
+    }
+
   }
 
   handlechange = async event => {
@@ -24,49 +95,45 @@ class App extends Component {
   }
 
 
-  addItem = async (event) => {
-    event.preventDefault();
 
-    const array = this.state.titleList;
-
-    array.push({ desc: this.state.title, status: "new" })
-
-    // await this.setState(prevState => ({
-    //   titleList: [...prevState.titleList, { desc: this.state.title, status: "new" }]
-    // }), this.setState({ title: "" }));
-
+  reloadPage = async () => {
+    const response = await axios.get("http://localhost:3000");
+    const array = response.data;
     array.sort(function (a, b) { return a.status.length - b.status.length });
 
     await this.setState({ title: "", titleList: array })
 
   }
 
+
+  addItem = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:3000/create", {
+      desc: this.state.title
+    })
+
+    this.reloadPage();
+
+  }
+
   strikeTask = async (index) => {
 
-    const array = this.state.titleList;
+    await axios.post("http://localhost:3000/update", {
+      id: this.state.titleList[index]._id
+    })
 
-    if (this.state.titleList[index].status === "new") {
-      array[index].status = "done"
+    this.reloadPage();
 
-    } else if (this.state.titleList[index].status === "done") {
-      array[index].status = "new"
-
-    }
-
-
-    array.sort(function (a, b) { return a.status.length - b.status.length });
-
-    await this.setState({ titleList: array })
   }
 
   removeTask = async (index) => {
 
-    const array = this.state.titleList;
-    array.splice(index, 1)
+    await axios.post("http://localhost:3000/delete", {
+      id: this.state.titleList[index]._id
+    })
 
-    array.sort(function (a, b) { return a.status.length - b.status.length });
-
-    await this.setState({ titleList: array })
+    this.reloadPage();
 
   }
 
@@ -77,9 +144,10 @@ class App extends Component {
 
       <h1>To-Do list</h1>
       <ul>{this.state.titleList.map((x, index) => {
-        if (x.status === "new") { return <li key={index} ><div onClick={() => this.removeTask(index)}><FontAwesomeIcon icon="times-circle" /></div><p onClick={() => this.strikeTask(index)}>{x.desc}</p></li> }
+        if (x.status === "new") { return <li key={index} draggable="true" onDragStart={() => { this.dragStart(index) }} onDragOver={(event) => event.preventDefault()} onDrop={() => { this.drop(index) }} ><div onClick={() => this.removeTask(index)}><FontAwesomeIcon icon="times-circle" /></div><p onClick={() => this.strikeTask(index)}>{x.desc}</p></li> }
         return <li key={index} ><div onClick={() => this.removeTask(index)}><FontAwesomeIcon icon="times-circle" /></div><p className="strike" onClick={() => this.strikeTask(index)}> {x.desc}</p></li>
-      })}</ul>
+      })}<li key="transparent" style={{ height: "15px" }} onDragOver={(event) => event.preventDefault()} onDrop={() => { this.drop("last") }}></li>
+      </ul>
       <form >
         <input type="text" placeholder="Titre" value={this.state.title} name="title" onChange={this.handlechange}></input>
         <button onClick={this.addItem}>Add task</button>
@@ -87,6 +155,13 @@ class App extends Component {
     </>
 
     )
+  }
+
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:3000");
+    const array = response.data;
+    array.sort(function (a, b) { return a.status.length - b.status.length });
+    await this.setState({ titleList: array })
   }
 
 }
